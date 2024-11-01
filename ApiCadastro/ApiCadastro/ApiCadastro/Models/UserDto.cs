5246e337208daa07e1e3450513fc79312de12464
LeoCaires03 <61246372+LeoCaires03@users.noreply.github.com>
2024-11-01 08:55:04 -0300

    Api Cadastro

---
+++ ApiCadastro/ApiCadastro/ApiCadastro/Models/UserDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiCadastro.Models
+{
+    public class UserDto
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string CEP { get; set; }
+    }
+}
